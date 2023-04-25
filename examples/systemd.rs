1b99219addd6e7482db5272e904872373e9ed9ae
gngpp <gngppz@gmail.com>
2023-04-25 18:06:21 +0800

    Update systemd example

---
+++ examples/systemd.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let child = std::process::Command::new("systemctl")
+        .arg("--help")
+        .output().unwrap();
+    let status = child.status;
+    println!("{:?}", status.success())
+}
