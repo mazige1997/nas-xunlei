777d710d976f9af24f826762a945491743a9b567
gngpp <gngppz@gmail.com>
2023-04-25 10:15:51 +0000

    Delete systemd example

--- examples/systemd.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let child = std::process::Command::new("systemctl")
-        .arg("--help")
-        .output()
-        .unwrap();
-    let status = child.status;
-    println!("{:?}", status.success())
-}
