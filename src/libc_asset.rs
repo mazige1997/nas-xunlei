b12c4e74121e55acaeacd4e407b88914971d302c
gngpp <gngppz@gmail.com>
2023-04-28 12:03:48 +0000

    Fix dynamic link library

--- src/libc_asset.rs
+++ src/libc_asset.rs
@@ -9,12 +9,16 @@ struct Asset;
 struct Asset;
 
 pub(crate) fn ld_env(envs: &mut std::collections::HashMap<String, String>) -> anyhow::Result<()> {
-    use crate::standard;
     use anyhow::Context;
+    #[cfg(target_arch = "x86_64")]
+    const LD: &str = "ld-linux-x86-64.so.2";
+    #[cfg(target_arch = "aarch64")]
+    const LD: &str = "ld-linux-aarch64.so.1";
+    const LD_PATH: &str = "/lib";
     const LD_LIBRARY_PATH: &str = "/tmp/libc";
     let libc_path = std::path::Path::new(LD_LIBRARY_PATH);
     if !libc_path.exists() {
-        standard::create_dir_all(libc_path, 0o755)?;
+        crate::standard::create_dir_all(libc_path, 0o755)?;
     }
     for filename in Asset::iter()
         .map(|v| v.into_owned())
@@ -22,7 +26,20 @@ pub(crate) fn ld_env(envs: &mut std::collections::HashMap<String, String>) -> an
     {
         let file = Asset::get(&filename).context("Failed to get bin asset")?;
         let target_file = libc_path.join(filename);
-        standard::write_file(&target_file, file.data, 0o755)?;
+        crate::standard::write_file(&target_file, file.data, 0o755)?;
+    }
+    unsafe {
+        let source_path = std::ffi::CString::new(
+            std::path::Path::new(LD_LIBRARY_PATH)
+                .join(LD)
+                .display()
+                .to_string(),
+        )?;
+        let target_path =
+            std::ffi::CString::new(std::path::Path::new(LD_PATH).join(LD).display().to_string())?;
+        if libc::symlink(source_path.as_ptr(), target_path.as_ptr()) != 0 {
+            anyhow::bail!(std::io::Error::last_os_error());
+        }
     }
     envs.insert(String::from("LD_LIBRARY_PATH"), LD_LIBRARY_PATH.to_string());
     Ok(())
